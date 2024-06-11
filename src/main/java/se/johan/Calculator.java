38d76d8df633ab9f9541262b8bec6a9b996baeba
Johan-Danielsson <johan.dunhill@gmail.com>
2024-06-11 09:36:45 +0200

    Added try with catch to not get error while putting in incorrect characters

--- src/main/java/se/johan/Calculator.java
+++ src/main/java/se/johan/Calculator.java
@@ -14,69 +14,60 @@ public class Calculator {
         // adding loop so you stay inside the program until option 5 is chosen
         while (true) {
             // creating the menu to chose option
+            System.out.println("---------------------------");
             System.out.println("Welcome to Basic Calculator");
-
-
             System.out.println("1: Addition");
             System.out.println("2: Subtraction");
             System.out.println("3: Multiplication");
             System.out.println("4: Division");
             System.out.println("5: Exit the calculator");
+            System.out.println("---------------------------");
             System.out.println("Please enter your option:");
 
             int option = 0;
+            // Added try and catch to not get error if you input characters
             try {
 
                 option = input.nextInt();
             } catch (InputMismatchException e) {
-                System.out.println("please enter a number.");
+                System.out.println("Please enter a number.");
                 input.next();
             }
 
             if (option == 1) {
-                System.out.print("Enter the first number: ");
+                System.out.println("Enter the first number: ");
                 double num1 = input.nextDouble();
-                System.out.print("Enter the second number: ");
+                System.out.println("Enter the second number: ");
                 double num2 = input.nextDouble();
 
                 addMethod(num1, num2); // passing relevant attributes to method
-            }
-            // This is without decimals, using int instead of double.
-            /*
-            {
-                System.out.print("Enter the first number: ");
-                int num1 = input.nextInt();
-                System.out.print("Enter the second number: ");
-                int num2 = input.nextInt();
-
-                addMethod(num1, num2); // passing relevant attributes to method
-            }
-             */
-            else if (option == 2) {
-                System.out.print("Enter the first number: ");
+            } else if (option == 2) {
+                System.out.println("Enter the first number: ");
                 double num1 = input.nextDouble();
-                System.out.print("Enter the second number: ");
+                System.out.println("Enter the second number: ");
                 double num2 = input.nextDouble();
 
                 subtractMethod(num1, num2); // passing relevant attributes to method
             } else if (option == 3) {
-                System.out.print("Enter the first number: ");
+                System.out.println("Enter the first number: ");
                 double num1 = input.nextDouble();
-                System.out.print("Enter the second number: ");
+                System.out.println("Enter the second number: ");
                 double num2 = input.nextDouble();
 
                 multiplyMethod(num1, num2); // passing relevant attributes to method
             } else if (option == 4) {
-                System.out.print("Enter the first number: ");
+                System.out.println("Enter the first number: ");
                 double num1 = input.nextDouble();
-                System.out.print("Enter the second number: ");
+                System.out.println("Enter the second number: ");
                 double num2 = input.nextDouble();
 
                 divisionMethod(num1, num2); // passing relevant attributes to method
             } else if (option == 5) {
+                System.out.println("---------------------------");
                 System.out.println("Thank you for using the calculator. Good bye!");
                 System.exit(0); // exit the program without
             } else {
+                System.out.println("---------------------------");
                 System.out.println("Please enter a correct value.");
             }
 
@@ -90,19 +81,14 @@ public class Calculator {
     // method for addition
     public static void addMethod(double num1, double num2) {
         double answer = num1 + num2;
+        System.out.println("---------------------------");
         System.out.println(num1 + "+" + num2 + "=" + answer);
-        // This is without decimals, using int instead of double.
-        /*
-        public static void addMethod(int num1, int num2){
-        int answer = num1+num2;
-        System.out.println(num1+ "+"+num2+ "=" + answer);
-         */
-
     }
 
     // method for subtraction
     public static void subtractMethod(double num1, double num2) {
         double answer = num1 - num2;
+        System.out.println("---------------------------");
         System.out.println(num1 + "-" + num2 + "=" + answer);
 
     }
@@ -110,6 +96,7 @@ public class Calculator {
     // method for multiplication
     public static void multiplyMethod(double num1, double num2) {
         double answer = num1 * num2;
+        System.out.println("---------------------------");
         System.out.println(num1 + "*" + num2 + "=" + answer);
 
     }
@@ -117,6 +104,7 @@ public class Calculator {
     // method for division
     public static void divisionMethod(double num1, double num2) {
         double answer = num1 / num2;
+        System.out.println("---------------------------");
         System.out.println(num1 + "/" + num2 + "=" + answer);
 
     }
